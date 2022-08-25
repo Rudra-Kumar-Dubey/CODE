f9579d900264f1e54d1ddc1ed2158494c848945a
Rudra Kumar Dubey <80637750+rudradubey04@users.noreply.github.com>
2022-08-25 14:47:20 +0530

    Add files via upload

---
+++ DSA/Move all negative elements to end.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    void segregateElements(int arr[],int n)
+    {
+        int negative[n] =  {};
+        int positive[n] =  {};
+        int a = 0;
+        int b = 0;
+        
+        for(int i = 0 ; i<n ; i++)
+        {
+            if(arr[i]<0)
+            {
+                negative[a] = arr[i];
+                a++;
+            }
+            else
+            {
+                positive[b] = arr[i];
+                b++;
+            }
+        }
+        for(int i=0; i<n; i++)
+        {
+            if(i<b)
+            {
+                arr[i] = positive[i] ;
+            }
+            else
+            {
+                arr[i] = negative[i-b];
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution ob;
+		ob.segregateElements(a,n);
+		
+        for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+		cout<<endl;
+	}
+}
+
+// } Driver Code Ends
